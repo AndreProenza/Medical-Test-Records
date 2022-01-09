9a572ce9bf11f4b268166d60cbd86877adec8df2
AndreProenza <proenza.1999@gmail.com>
2022-01-09 12:58:20 +0000

    [ADD] Laboratory complete

---
+++ Code/Laboratory/src/main/java/com/laboratory/controller/LoginController.java
@@ -0,0 +1,42 @@
+package com.laboratory.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.laboratory.model.mongodb.Citizen;
+import com.laboratory.service.CitizenService;
+
+@Controller
+@RequestMapping("login")
+public class LoginController {
+	
+	private CitizenService citizenService;
+	
+	public LoginController(CitizenService citizenService) {
+		super();
+		this.citizenService = citizenService;
+	}
+	
+	@GetMapping("")
+	public String showLoginForm(Model model) {
+		Citizen citizen = new Citizen();
+		model.addAttribute("citizen", citizen);
+		return "login";
+	}
+	
+
+	@PostMapping("")
+	public String submitLoginForm(@ModelAttribute("citizen") @Valid Citizen citizen, Errors errors) {
+		if(errors.hasFieldErrors("id") && errors.hasFieldErrors("password")) {
+			return "login";
+		}
+		Citizen existingCitizen = citizenService.login(citizen.getId(), citizen.getPassword());
+	    return existingCitizen == null ? "login" : "home";
+	}
+}
---
+++ Code/Laboratory/src/main/java/com/laboratory/utils/PasswordGenerator.java
@@ -0,0 +1,50 @@
+package com.laboratory.utils;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+public final class PasswordGenerator {
+
+    private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String DIGITS = "0123456789";
+    private static final String PUNCTUATION = "@$!%*?&";
+
+
+    /**
+     * This method will generate a password using secure random and a combination
+     * of upper and lower case, numbers and punctuation.
+     *
+     * @param length the length of the password that will be generated
+     * @return the generated password or empty string on length less or equal to 0
+     * 
+     * based on https://stackoverflow.com/questions/19743124/java-password-generator
+     */
+    public static String generate(int length) {
+        if (length <= 0) {
+            return "";
+        }
+
+        // Variables
+        StringBuilder password = new StringBuilder(length);
+        SecureRandom random = new SecureRandom();
+
+        // Collect the categories to use
+        List<String> charCategories = new ArrayList<>(4);
+        
+        // add the possible characters
+        charCategories.add(LOWER);
+        charCategories.add(UPPER);
+        charCategories.add(DIGITS);
+        charCategories.add(PUNCTUATION);
+
+        // Create the password
+        for (int i = 0; i < length; i++) {
+            String charCategory = charCategories.get(random.nextInt(charCategories.size()));
+            int position = random.nextInt(charCategory.length());
+            password.append(charCategory.charAt(position));
+        }
+        return new String(password);
+    }
+}
