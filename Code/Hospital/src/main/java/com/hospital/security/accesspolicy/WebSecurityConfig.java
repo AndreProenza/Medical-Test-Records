73021ada73c5511b9a89f930c8e49b52857bd7a6
Ana Albuquerque <fc53512@alunos.fc.ul.pt>
2022-01-11 11:29:27 +0000

    Auth

--- Code/Hospital/src/main/java/com/hospital/security/accesspolicy/WebSecurityConfig.java
+++ Code/Hospital/src/main/java/com/hospital/security/accesspolicy/WebSecurityConfig.java
@@ -41,7 +41,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().anyRequest().authenticated()
 		.and().formLogin().permitAll()
-		.and().logout().permitAll();
+		.and().logout().permitAll()
+		.and().exceptionHandling().accessDeniedPage("/403");
 	}
 	
 	/*
