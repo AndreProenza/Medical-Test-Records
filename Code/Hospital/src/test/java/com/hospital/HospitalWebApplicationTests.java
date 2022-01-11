0a8ef349af5fb917a7873a93c899f99f875151d5
Ana Albuquerque <fc53512@alunos.fc.ul.pt>
2022-01-11 11:02:58 +0000

    TODO

--- Code/Hospital/src/test/java/com/hospital/HospitalWebApplicationTests.java
+++ Code/Hospital/src/test/java/com/hospital/HospitalWebApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HospitalApplicationTests {
+class HospitalWebApplicationTests {
 
 	@Test
 	void contextLoads() {
