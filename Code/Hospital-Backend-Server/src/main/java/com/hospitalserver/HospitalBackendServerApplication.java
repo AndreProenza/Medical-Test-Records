49cc070258292768b6bd9a644a940f936fcad020
Joel Russo <joelrussopt@gmail.com>
2022-01-17 22:48:46 +0000

    Added empty Hospital-backend-server

---
+++ Code/Hospital-Backend-Server/src/main/java/com/hospitalserver/HospitalBackendServerApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalBackendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalBackendServerApplication.class, args);
+	}
+
+}
