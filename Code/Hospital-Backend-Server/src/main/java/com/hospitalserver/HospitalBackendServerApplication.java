7d209349c7f57b94b1c22cfb03282a8273131f60
Joel Russo <joelrussopt@gmail.com>
2022-01-18 01:06:42 +0000

    Added begining of server of HBS

--- Code/Hospital-Backend-Server/src/main/java/com/hospitalserver/HospitalBackendServerApplication.java
+++ Code/Hospital-Backend-Server/src/main/java/com/hospitalserver/HospitalBackendServerApplication.java
@@ -3,11 +3,14 @@ package com.hospitalserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.hospitalserver.server.Server;
+
 @SpringBootApplication
 public class HospitalBackendServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalBackendServerApplication.class, args);
+		Server.run();
 	}
 
 }
